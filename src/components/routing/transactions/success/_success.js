04d7c185d18aeaef930ea123659d9fac82ab1fe5
Janeal Pimentel <ceejaypimentel@hotmail.ca>
2020-04-23 15:04:43 -0500

    Fixed transaction issue of only showing low amounts of total

--- src/components/routing/transactions/success/_success.js
+++ src/components/routing/transactions/success/_success.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 // import { setCookie, getCookie, removeCookie } from 'react-cookies';
 import queryString from 'query-string';
 import Axios from 'axios';
+import { Link } from 'react-router-dom';
 
 import {
   Paper,
@@ -11,12 +12,12 @@ import useStyles from './styles/styles';
 
 const Success = (props, { history }) => {
   const classes = useStyles();
-  const [orderDetail, setOrderDetail] = useState([JSON.parse(sessionStorage.getItem("orderDetail"))]);
+  const [orderDetail, setOrderDetail] = useState(JSON.parse(sessionStorage.getItem("orderDetail")));
 
   useEffect(() => {
     const fetchOrderDetailData = () => {
       const sessionId = JSON.parse(sessionStorage.getItem("sessionId"));
-      if (queryString.parse(props.location.search).session_id !== sessionId) {
+      if (queryString.parse(props.location.search).session_id !== sessionId || sessionId === null) {
         Axios.get("http://localhost:3000/checkout/success", {
           params: {
             cart: JSON.stringify(JSON.parse(queryString.parse(props.location.search).cart)),
@@ -43,17 +44,23 @@ const Success = (props, { history }) => {
 
   return (
     <div className={classes.root}>
-      {console.log(orderDetail[0])}
-      <Paper className={classes.paper}>
-        <Typography>Transaction Number: #{orderDetail[0].order.reference_number}</Typography>
-        <Typography>Total: ${orderDetail[0].order.total}</Typography>
-        <div>
-          <Typography>Items:</Typography>
-          {orderDetail[0].products.map(product => (
-            <Typography key={product.id}>{product.name} </Typography>
-          ))}
-        </div>
-      </Paper>
+      {console.log(orderDetail)}
+      {orderDetail !== undefined && orderDetail !== null ?
+        <Paper className={classes.paper}>
+          <Typography variant='h4' className={classes.successTitle}>Transaction Successful</Typography>
+          <Typography variant="subtitle2" className={classes.thankYou}>Thank you for choosing <span className={classes.shop}>The Prints</span></Typography>
+          <div className={classes.content}>
+            <Typography className={classes.number} variant="h6">Transaction Number: #<span className={classes.number__item}>{orderDetail.order.reference_number}</span></Typography>
+            <div>
+              <Typography className={classes.titleItem} variant='h6'>Items:</Typography>
+              {orderDetail.products.map((product, index) => (
+                <Typography key={product.id} className={classes.item}><span>{product.name}</span><span className={classes.grow} />$<span className={classes.itemPrice}>{orderDetail.order_details[index].price}</span><Link to={`/product/${product.id}`}>info</Link></Typography>
+              ))}
+            </div>
+            <div className={classes.grow} />
+            <Typography className={classes.total}><span>Total:</span><span className={classes.grow} /><span>${orderDetail.order.total}</span></Typography>
+          </div>
+        </Paper> : <></>}
     </div>
   )
 }
--- src/components/routing/transactions/success/styles/styles.js
+++ src/components/routing/transactions/success/styles/styles.js
@@ -15,6 +15,54 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.down('md')]: {
       width: '100%'
     },
+  },
+  successTitle: {
+    alignSelf: 'center',
+    fontStyle: 'italic',
+    padding: '20px 0px 0px 0px',
+    fontSize: '3rem'
+  },
+  content: {
+    padding: 10,
+    margin: '0 auto',
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%'
+  },
+  number: {
+    fontWeight: 500
+  },
+  number__item: {
+    color: 'red'
+  },
+  shop: {
+    color: 'red',
+    fontStyle: 'italic',
+    fontSize: '1rem'
+  },
+  thankYou: {
+    alignSelf: 'center',
+  },
+  grow: {
+    flexGrow: 1
+  },
+  total: {
+    fontSize: '1.5rem',
+    display: 'flex'
+  },
+  item: {
+    fontSize: '1.2rem',
+    display: 'flex'
+  },
+  titleItem: {
+    paddingTop: 20,
+    fontStyle: 'italic',
+    fontWeight: 600
+  },
+  itemPrice: {
+    paddingRight: 5,
+    borderRight: 'solid black 2px',
+    marginRight: 5
   }
 }))
 
